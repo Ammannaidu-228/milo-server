fbd2c6beee4c2275930878d948040386c6e46332
Aman228-dev <ammannaidu.gollapalli.228@gmail.com>
2025-10-24 22:19:45 +0530

    cookie - parser changed

--- src/server.js
+++ src/server.js
@@ -12,11 +12,13 @@ import { app, server } from "./lib/socket.js";
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
-
+app.use(cookieParser());
 app.use(express.json({ limit: "5mb" })); // req.body
 // app.use(cors());
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
-app.use(cookieParser());
+app.use(cors(
+  { origin: ENV.CLIENT_URL, 
+    credentials: true })
+  );
 
 
 app.get("/hello",(req,res)=>{
@@ -29,14 +31,14 @@ app.use("/messages", messageRoutes);
 
 // make ready for deployment
 // Serve the built client from the `client/dist` folder (was referencing ../frontend which doesn't exist)
-if (ENV.NODE_ENV === "production") {
-  const clientDistPath = path.join(__dirname, "../client/dist");
-  app.use(express.static(clientDistPath));
+// if (ENV.NODE_ENV === "production") {
+//   const clientDistPath = path.join(__dirname, "../client/dist");
+//   app.use(express.static(clientDistPath));
 
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(clientDistPath, "index.html"));
-  });
-}
+//   app.get("*", (_, res) => {
+//     res.sendFile(path.join(clientDistPath, "index.html"));
+//   });
+// }
 
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/hello`);
